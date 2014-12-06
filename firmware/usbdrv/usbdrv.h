a41d2fb04e9816890e2a122bba40306ba8382cef
Stephan Baerwolf <stephan.baerwolf@tu-ilmenau.de>
2014-12-06 13:51:09 +0100

    migrate to new v-usb driver release 20131128
    The mainline master-HEAD commit 6f64e80f219273967d6ac7096f3ca3d7db5a0e79
    was used.
    
    https://github.com/obdev/v-usb/commit/6f64e80f219273967d6ac7096f3ca3d7db5a0e79
    
    Signed-off-by: Stephan Baerwolf <stephan.baerwolf@tu-ilmenau.de>

--- firmware/usbdrv/usbdrv.h
+++ firmware/usbdrv/usbdrv.h
@@ -117,6 +117,23 @@ USB messages, even if they address another (low-speed) device on the same bus.
 
 */
 
+
+#ifdef __cplusplus
+// This header should be included as C-header from C++ code. However if usbdrv.c
+// is incorporated into a C++ module with an include, function names are mangled
+// and this header must be parsed as C++ header, too. External modules should be
+// treated as C, though, because they are compiled separately as C code.
+extern "C" {
+#endif
+
+#include "usbconfig.h"
+#include "usbportability.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+
 /* ------------------------------------------------------------------------- */
 /* --------------------------- Module Interface ---------------------------- */
 /* ------------------------------------------------------------------------- */
